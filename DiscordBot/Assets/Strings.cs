c971eef12cb7ebd3c71bba3c33d611f6d7975cc9
Avery Torrance <averytorrance1107@gmail.com>
2024-01-09 21:49:40 -0600

    Corrected code comment text

--- DiscordBot/Assets/Strings.cs
+++ DiscordBot/Assets/Strings.cs
@@ -3,7 +3,7 @@
     public static class Strings
     {
         /// <summary>
-        /// Generates the string for a discord message
+        /// Generates the string for a discord mention
         /// </summary>
         /// <param name="userID"></param>
         /// <returns></returns>
